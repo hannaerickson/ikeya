52fb65c1bb3c7b9e2f23385b7c9e8ea77a7a0eb5
Melipo <Melipo85@gmail.com>
2023-01-09 17:21:10 -0500

    Authenticator now working

--- design_service/routers/accounts.py
+++ design_service/routers/accounts.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter, Depends, Response, Request, HTTPException, status
+from fastapi import (
+    APIRouter,
+    Depends,
+    Response,
+    Request,
+    HTTPException,
+    status,
+)
 from typing import Union, List
 from queries.accounts import AccountIn, AccountOut, AccountsQueries
 from jwtdown_fastapi.authentication import Token
@@ -7,20 +14,24 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+
 class AccountForm(BaseModel):
     username: str
     password: str
 
+
 class AccountToken(Token):
     account: AccountOut
+
 
 class HttpError(BaseModel):
     detail: str
 
+
 @router.get("/token", response_model=AccountToken | None)
 async def get_token(
     request: Request,
-    account: AccountOut = Depends(authenticator.try_get_current_account_data)
+    account: AccountOut = Depends(authenticator.try_get_current_account_data),
 ) -> AccountToken | None:
     if authenticator.cookie_name in request.cookies:
         return {
@@ -29,21 +40,22 @@ async def get_token(
             "account": account,
         }
 
+
 @router.post("/api/accounts", response_model=AccountToken | HttpError)
 async def create_account(
     info: AccountIn,
     request: Request,
     response: Response,
-    repo: AccountsQueries = Depends(),
+    accounts: AccountsQueries = Depends(),
 ):
     hashed_password = authenticator.hash_password(info.password)
     try:
-        account = repo.create(info, hashed_password)
+        account = accounts.create(info, hashed_password)
     except:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Cannot create an account with those credentials",
         )
     form = AccountForm(username=info.username, password=info.password)
-    token = await authenticator.login(response, request, form, repo)
+    token = await authenticator.login(response, request, form, accounts)
     return AccountToken(account=account, **token.dict())
--- design_service/routers/rooms.py
+++ design_service/routers/rooms.py
@@ -6,33 +6,50 @@ from authenticator import authenticator
 
 router = APIRouter()
 
-@router.get("/api/rooms", response_model=Union[List[RoomOut], Error], tags=["Rooms"])
+
+@router.get(
+    "/api/rooms", response_model=Union[List[RoomOut], Error], tags=["Rooms"]
+)
 def get_all_rooms(
     repo: RoomRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ):
-        return repo.get_all_rooms()
+    return repo.get_all_rooms()
 
 
-@router.post("/api/rooms", response_model=Union[RoomOut, Error], tags=["Rooms"])
-def create_room(room: RoomIn, response: Response, repo: RoomRepository = Depends()):
+@router.post(
+    "/api/rooms", response_model=Union[RoomOut, Error], tags=["Rooms"]
+)
+def create_room(
+    room: RoomIn,
+    response: Response,
+    repo: RoomRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
     if room is None:
         response.status_code = 400
     return repo.create(room)
 
-@router.get("/api/rooms/{room_id}", response_model=Optional[RoomOut], tags=["Rooms"])
+
+@router.get(
+    "/api/rooms/{room_id}", response_model=Optional[RoomOut], tags=["Rooms"]
+)
 def get_one_room(
     room_id: int,
     response: Response,
     repo: RoomRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> RoomOut:
     room = repo.get_one_room(room_id)
     if room is None:
         response.status_code = 404
     return room
 
+
 @router.delete("/api/rooms/{room_id}", response_model=bool, tags=["Rooms"])
 def delete_room(
     room_id: int,
     repo: RoomRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> bool:
     return repo.delete(room_id)
