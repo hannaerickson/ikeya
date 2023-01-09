7f1ffef281def8685fee45d01d49269610b4b738
hanna <hmae221@gmail.com>
2023-01-09 17:43:50 -0600

    Minor changes

--- design_service/routers/rooms.py
+++ design_service/routers/rooms.py
@@ -16,28 +16,7 @@ def get_all_rooms(
 ):
     return repo.get_all_rooms()
 
-<<<<<<< HEAD
 @router.get("/api/rooms/{room_id}", response_model=Optional[RoomOut], tags=["Rooms"])
-=======
-
-@router.post(
-    "/api/rooms", response_model=Union[RoomOut, Error], tags=["Rooms"]
-)
-def create_room(
-    room: RoomIn,
-    response: Response,
-    repo: RoomRepository = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data),
-):
-    if room is None:
-        response.status_code = 400
-    return repo.create(room)
-
-
-@router.get(
-    "/api/rooms/{room_id}", response_model=Optional[RoomOut], tags=["Rooms"]
-)
->>>>>>> 45cb7cadba1b5851d9067d632d6e5ad39be71e12
 def get_one_room(
     room_id: int,
     response: Response,
@@ -49,7 +28,6 @@ def get_one_room(
         response.status_code = 404
     return room
 
-<<<<<<< HEAD
 @router.post("/api/rooms", response_model=Union[RoomOut, Error], tags=["Rooms"])
 def create_room(room: RoomIn, response: Response, repo: RoomRepository = Depends()):
     if room is None:
@@ -63,8 +41,6 @@ def update_room(
     repo: RoomRepository = Depends(),
 ) -> Union[RoomOut, Error]:
     return repo.update(room_id, room)
-=======
->>>>>>> 45cb7cadba1b5851d9067d632d6e5ad39be71e12
 
 @router.delete("/api/rooms/{room_id}", response_model=bool, tags=["Rooms"])
 def delete_room(
