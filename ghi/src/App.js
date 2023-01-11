9ad685c4192a805cc75738d1244358f2d56a0140
Abdoul <105816253+doulayea@users.noreply.github.com>
2023-01-11 15:38:36 -0500

    Some errors on the frontend side fixed

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoomsList from "./Rooms/RoomsList";
 import LoginForm from "./Accounts/LoginForm";
-import { AuthProvider, useToken } from "./Auth";
+import { AuthProvider, useToken } from "./Accounts/Auth";
 
 function GetToken() {
   // Get token from JWT cookie (if already logged in)
@@ -11,17 +11,17 @@ function GetToken() {
 
 function App() {
   return (
-    <AuthProvider>
-      <GetToken />
-      <BrowserRouter>
-        <div className="container">
-          <Routes>
-            <Route path="/rooms" element={<RoomsList />} />
-            <Route path="/login" element={<LoginForm />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <GetToken />
+          <div className="container">
+            <Routes>
+              <Route path="/rooms" element={<RoomsList />} />
+              <Route path="/login" element={<LoginForm />} />
+            </Routes>
+          </div>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
--- journals/Abdoul.md
+++ journals/Abdoul.md
@@ -6,3 +6,6 @@ Created some design service routes and queries for our project. Had a bit of a r
 
 ### January 6th:
 Were making authentication tokens for our login and logout routes, which was a real pain in the butt. After some clarification though, we were able to make a breakthrough and get them mostly working. Just have to iron out a few kinks and we'll be good to go.
+
+#### January 9th:
+Fixed that pesky delete function today. Just had to add ON DELETE CASCADE to our table file, as we had a foreign key which was affecting our delete function.
