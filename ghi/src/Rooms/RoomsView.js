d9d40675ec926e6d46008d5d5663c0a49aa168ec
Melipo <Melipo85@gmail.com>
2023-01-19 17:00:19 -0500

    RoomView now pulls data

--- ghi/src/Rooms/RoomsView.js
+++ ghi/src/Rooms/RoomsView.js
@@ -1,193 +1,61 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useAuthContext } from "../Accounts/Auth";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useAuthContext } from "../Accounts/Auth";
 
 export default function RoomView() {
   const location = useLocation();
   const roomData = location.state;
+  const { token, setToken } = useAuthContext();
+  const [furnitures, setFurnitures] = useState([]);
 
-  const [furniture, setFurniture] = useState("");
-  const [furniture_name, setFurnitureName] = useState("");
-  const [furniture_picture, setFurniturePictureUrl] = useState("");
-
-  const [room, setRoom] = useState("");
-  const [room_id, setRoomId] = useState("");
-  const [room_name, setRoomName] = useState("");
-  const [room_description, setRoomDescription] = useState("");
-
-  const [show, setShow] = useState(false);
-
-  const { token } = useAuthContext();
-
-  const fetchFurnitureData = async () => {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${roomData}/furniture`;
-    const response = await fetch(url, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    if (response.ok) {
-      const data = await response.json();
-      setFurniture(data);
+  const getToken = async () => {
+    const storedToken = localStorage.getItem("token");
+    if (!storedToken) {
+      localStorage.setItem("token", token);
+    } else {
+      setToken(storedToken);
     }
   };
-
-  const fetchRoomData = async () => {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${roomData}`;
-    const response = await fetch(url, {
-      "Content-Type": "application/json",
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    if (response.ok) {
-      const data = await response.json();
-      setRoom(data);
-    }
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = {
-      name: furniture_name,
-      picture_url: furniture_picture,
-      room_id: room_id,
-    };
-
-    const furnitureUrl = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/furniture/`;
-    const fetchConfig = {
-      method: "post",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    };
-
-    const response = await fetch(furnitureUrl, fetchConfig);
-    if (response.ok) {
-      const newFurniture = await response.json();
-      console.log(newFurniture);
-      fetchFurnitureData();
-    }
-  };
-
-  const handleDelete = async (furniture_id) => {
-    const resp = await fetch(
-      `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${roomData}/furniture/${furniture_id}`,
+  const getData = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${roomData}/furniture`,
       {
-        method: "DELETE",
         headers: { Authorization: `Bearer ${token}` },
       }
     );
-    if (resp.ok) {
-      fetchFurnitureData();
+
+    if (response.ok) {
+      const data = await response.json();
+      setFurnitures(data);
     }
   };
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   useEffect(() => {
-    fetchRoomData();
-    fetchFurnitureData();
+    getData();
+    getToken();
   }, [token]);
 
   return (
     <>
-      <br></br>
-      <h1>room.name</h1>
-      <h2>room.description</h2>
-      <br></br>
-      <br></br>
-      <Container>
-        <Row>
-          {furniture.length === 0 ? (
-            <div>No Furniture Found</div>
-          ) : (
-            furniture.map((furniture) => (
-              <Col key={furniture.id}>
-                <Card className="bg-black text-white">
-                  <Card.Img variant="top" src={furniture.picture_url} />
-                  <Card.Body>
-                    <div className="text-center">
-                      <Card.Text className="text-right">
-                        {furniture_name}{" "}
-                      </Card.Text>
-
-                      <Button
-                        variant="outline-danger"
-                        className="text-right"
-                        onClick={() => {
-                          handleDelete(furniture.id);
-                        }}
-                      >
-                        Delete from room
-                      </Button>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))
-          )}
-        </Row>
-        <br></br>
-        <br></br>
-        <br></br>
-        <Row>{/* ... */}</Row>
-      </Container>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Button
-          type="button"
-          variant="outline-success"
-          size="lg"
-          onClick={handleShow}
-        >
-          Add Furniture to Room
-        </Button>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Furniture</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="Name"
-                value={furniture_name}
-                onChange={(e) => setFurnitureName(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Picture URL"
-                value={furniture_picture}
-                onChange={(e) => setFurniturePictureUrl(e.target.value)}
-              />
-              <button type="submit">Submit</button>
-            </form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      <h1>Room View</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Picture</th>
+          </tr>
+        </thead>
+        <tbody>
+          {furnitures?.map((furniture) => {
+            return (
+              <tr key={furniture.id}>
+                <td>{furniture.name}</td>
+                <td>{furniture.picture_url}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
