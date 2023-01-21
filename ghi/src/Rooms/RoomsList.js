0178703d145b87befa49ee3fe5d5c5c424fce998
Tyler Byrd <tbyrd2237@gmail.com>
2023-01-21 14:17:49 -0500

    Removed the Link import

--- ghi/src/Rooms/RoomsList.js
+++ ghi/src/Rooms/RoomsList.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../Accounts/Auth";
-import { Link } from "react-router-dom";
 
 //Styling
 import Card from "react-bootstrap/Card";
