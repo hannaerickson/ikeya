a47626491261bb14e676682868cd7d7ff0092151
Melipo <Melipo85@gmail.com>
2023-01-26 11:37:33 -0500

    reverted changes to dashboard and updateform

--- ghi/src/ModalForms/UpdateRoom.js
+++ ghi/src/ModalForms/UpdateRoom.js
@@ -2,58 +2,33 @@ import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../Accounts/Auth";
 import { useLocation } from "react-router-dom";
 
-const UpdateRoomForm = ({ room, roomId, onSelectRoom, onSubmit }) => {
+function UpdateRoomForm() {
   const { token } = useContext(AuthContext);
-  const location = useLocation();
-  const roomData = location.state;
-
-  const [name, setName] = useState(room ? room.name : "");
-  const [description, setDescription] = useState(room ? room.description : "");
-  const [picture_url, setPictureUrl] = useState(room ? room.picture_url : "");
-
-  const [room_id, setRoomId] = useState(roomId || "");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [picture_url, setPictureUrl] = useState("");
+  const [room_id, setRoomId] = useState("");
   const [username, setUserName] = useState(null);
   const [rooms, setRooms] = useState([]);
   const [show, setShow] = useState(true);
 
-  const handlePictureChange = (event) => {
-    setPictureUrl(event.target.value);
+  const handleRoomChange = (e) => {
+    setRoomId(e.target.value);
   };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = { name, description, picture_url, username };
-    const furnitureUrl = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${room_id}`;
-    const response = await fetch(furnitureUrl, {
-      method: "PUT",
-      credentials: "include",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (response.ok) {
-      const updateRoom = await response.json();
-      console.log(updateRoom);
-
-      setName("");
-      setDescription("");
-      setPictureUrl("");
-      setUserName();
-      handleClose();
-      window.location.reload();
-    }
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
-
-  const handleClose = () => setShow(false);
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+  const handlePictureChange = (e) => {
+    setPictureUrl(e.target.value);
+  };
 
   const fetchData = async () => {
     const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/me`;
     const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+      headers: { Authorization: `Bearer ${token}` },
     });
     if (response.ok) {
       const data = await response.json();
@@ -75,60 +50,96 @@ const UpdateRoomForm = ({ room, roomId, onSelectRoom, onSubmit }) => {
     fetchData();
   }, [username]);
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const data = { name, description, picture_url, username };
+    const furnitureUrl = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${room_id}`;
+    const response = await fetch(furnitureUrl, {
+      method: "PUT",
+      credentials: "include",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.ok) {
+      const newFurniture = await response.json();
+      setName("");
+      setDescription("");
+      setPictureUrl("");
+      setUserName();
+      setShow(false);
+      window.location.reload();
+    }
+  }
+
   return (
     <div className="row">
-      <div className="offset-3 col-6">
-        <div className="shadow p-4 mt-4">
-          <h1 style={{ fontSize: "25px" }}>Update Your Room</h1>
-          <form onSubmit={handleSubmit} id="create-furniture-form">
-            <div className="form-floating mb-3">
-              <input
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
-                required
-                type="text"
-                name="name"
-                id="name"
-                value={name}
-                className="form-control"
-              />
-              <label htmlFor="name">Name</label>
-            </div>
-            <div className="form-floating mb-3">
-              <textarea
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder="picture_url"
-                required
-                type="text"
-                name="picture_url"
-                id="picture_url"
-                value={description}
-                className="form-control"
-              />
-              <label htmlFor="picture_url">Description</label>
-            </div>
-            <div className="form-floating mb-3">
-              <input
-                onChange={handlePictureChange}
-                placeholder="picture_url"
-                required
-                type="text"
-                name="picture_url"
-                id="picture_url"
-                value={picture_url}
-                className="form-control"
-              />
-              <label htmlFor="picture_url">Picture</label>
-            </div>
-            <div className="mb-3"></div>
-            <button className="btn btn-outline-success d-block mx-auto">
-              Update!
-            </button>
-          </form>
-        </div>
+      <div className="p-3">
+        <h1>Update Your Room</h1>
+        <br />
+        <form onSubmit={handleSubmit} id="update-room-form">
+          <div className="form-floating mb-3">
+            <input
+              onChange={handleNameChange}
+              placeholder="Name"
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              className="form-control"
+            />
+            <label htmlFor="name">Room Name</label>
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="description">Description</label>
+            <textarea
+              onChange={handleDescriptionChange}
+              placeholder="Updated description"
+              type="textarea"
+              name="description"
+              id="description"
+              value={description}
+              className="form-control"
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              onChange={handlePictureChange}
+              placeholder="picture_url"
+              type="text"
+              name="picture_url"
+              id="picture_url"
+              value={picture_url}
+              className="form-control"
+            />
+            <label htmlFor="picture_url">Picture URL</label>
+          </div>
+          <div className="mb-3">
+            <select
+              onChange={handleRoomChange}
+              required
+              name="room_id"
+              id="room_id"
+              value={room_id}
+              className="form-select"
+            >
+              <option value="">Which room would you like to update?</option>
+              {rooms.map((room) => {
+                return (
+                  <option value={room.id} key={room.id}>
+                    {room.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <button className="btn btn-yellow d-block mx-auto">Update!</button>
+        </form>
       </div>
     </div>
   );
-};
+}
 
 export default UpdateRoomForm;
--- ghi/src/Views/Dashboard.js
+++ ghi/src/Views/Dashboard.js
@@ -1,34 +1,34 @@
+//Function
 import React, { useState, useEffect } from "react";
 import { useAuthContext } from "../Accounts/Auth";
 import { Link } from "react-router-dom";
+
+//Form
 import UpdateRoomForm from "../ModalForms/UpdateRoom";
 import RoomsForm from "../ModalForms/RoomsForm";
-import { Modal } from "react-bootstrap";
-import "../CSS/Style.css";
+import FurnitureForm from "../ModalForms/FurnitureForm";
+
+//Styling
 import Button from "react-bootstrap/Button";
 import Swal from "sweetalert2";
+import { Modal } from "react-bootstrap";
+import "../CSS/Style.css";
 
 export default function Dashboard() {
   const [list, setList] = useState([]);
-  const { token } = useAuthContext();
-  const [showRoom, setShowRoom] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [showUpdate, setShowUpdate] = useState(false);
-  const [selectedRoom, setSelectedRoom] = useState(null);
   const [username, setUsername] = useState(null);
-  const [roomId, setRoomId] = useState(null);
+  const { token } = useAuthContext();
+  const { handleSubmit } = useState();
+  const [showRoom, setShowRoom] = useState(false);
+  const [showFurniture, setShowFurniture] = useState(false);
+  const [showUpdate, setShowUpdate] = useState(false);
 
   const handleCloseRoom = () => setShowRoom(false);
   const handleShowRoom = () => setShowRoom(true);
-  const handleCloseUpdate = () => {
-    setShowUpdate(false);
-    setSelectedRoom(null);
-  };
-  const handleCloseModal = () => setShowModal(false);
-  const handleShowModal = () => setShowModal(true);
-  const handleSubmit = async (event) => {
-    // Handle form submission
-  };
+  const handleCloseFurniture = () => setShowFurniture(false);
+  const handleShowFurniture = () => setShowFurniture(true);
+  const handleCloseUpdate = () => setShowUpdate(false);
+  const handleShowUpdate = () => setShowUpdate(true);
 
   const fetchData = async () => {
     const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/me`;
@@ -41,21 +41,14 @@ export default function Dashboard() {
     }
   };
 
-  const handleDelete = async (id) => {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${id}`;
-    const resp = await fetch(url, {
+  const deletion = async (id) => {
+    const urlDelete = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/rooms/${id}`;
+    const resp = await fetch(urlDelete, {
       headers: { Authorization: `Bearer ${token}` },
       method: "DELETE",
     });
-    if (resp.ok) {
-      fetchData();
-      Swal.fire("Deleted!", "Your room has been deleted.", "success");
-    }
-  };
-
-  const handleSelectRoom = (room) => {
-    setSelectedRoom(room);
-    setRoomId(room.id);
+    const data = await resp.json();
+    fetchData();
   };
 
   useEffect(() => {
@@ -68,89 +61,99 @@ export default function Dashboard() {
         .then((res) => setUsername(res.account.username));
     }
     fetchData();
-  }, [token, username]);
+  }, [token, username, list]);
 
   return (
-    <div className="d-grid gap-2">
-      <div className="btn-group">
-        <button onClick={handleShowRoom} className="btn btn-info m-1">
-          Create A Room
-        </button>
-        <Modal show={showRoom} onHide={handleCloseRoom}>
-          <Modal.Body>
-            <RoomsForm handleSubmit={handleSubmit} />
-          </Modal.Body>
-        </Modal>
-      </div>
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        {list?.map((room) => (
-          <div className="col" key={room.id}>
-            <div className="card bg-light mb-3 text-center h-100">
-              <div className="card-header d-flex justify-content-between">
-                <button className="btn btn-outline-primary btn-sm m-1">
-                  <Link
-                    to="/rooms/furniture"
-                    state={room.id}
-                    style={{ textDecoration: "none", color: "black" }}
-                  >
-                    Furniture
-                  </Link>
-                </button>
-                <div>
-                  <button
-                    onClick={handleShowModal}
-                    className="btn btn-warning m-1"
-                  >
-                    Update
-                  </button>
-                  <Modal show={showModal} onHide={handleCloseModal}>
-                    <UpdateRoomForm
-                      room={selectedRoom}
-                      roomId={room.id}
-                      onSelectRoom={handleSelectRoom}
-                      onSubmit={handleSubmit}
-                    />
-                  </Modal>
-                  <Button
-                    variant="outline-danger btn-sm m-1"
-                    className="text-right"
-                    onClick={() => {
-                      Swal.fire({
-                        title: "Are you sure?",
-                        text: "You won't be able to revert this!",
-                        icon: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#3085d6",
-                        cancelButtonColor: "#d33",
-                        confirmButtonText: "Yes, delete it!",
-                      }).then((result) => {
-                        if (result.value) {
-                          handleDelete(room.id);
-                          Swal.fire(
-                            "Deleted!",
-                            "Your room has been deleted.",
-                            "success"
-                          );
-                        }
-                      });
-                    }}
-                  >
-                    Delete
-                  </Button>
+    <div>
+      <br />
+      <h1>Welcome back, {username}</h1>
+      <div className="d-grid gap-2">
+        <div className="btn-group">
+          <button onClick={handleShowRoom} className="btn btn-orange m-1">
+            Create A Room
+          </button>
+          <Modal show={showRoom} onHide={handleCloseRoom}>
+            <Modal.Body>
+              <RoomsForm handleSubmit={handleSubmit} />
+            </Modal.Body>
+          </Modal>
+
+          <button onClick={handleShowFurniture} className="btn btn-green m-1">
+            Create Furniture
+          </button>
+          <Modal show={showFurniture} onHide={handleCloseFurniture}>
+            <Modal.Body>
+              <FurnitureForm handleSubmit={handleSubmit} />
+            </Modal.Body>
+          </Modal>
+
+          <button onClick={handleShowUpdate} className="btn btn-yellow m-1">
+            Update A Room
+          </button>
+          <Modal show={showUpdate} onHide={handleCloseUpdate}>
+            <Modal.Body>
+              <UpdateRoomForm handleSubmit={handleSubmit} />
+            </Modal.Body>
+          </Modal>
+        </div>
+        <br />
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {list?.map((room) => {
+            return (
+              <div className="col" key={room.id}>
+                <div className="card bg-light mb-3 text-center h-100">
+                  <img
+                    src={room.picture_url}
+                    className="card-img-top card-image"
+                    alt="Image"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{room.name}</h5>
+                    <p className="card-text crop-text-1">{room.description}</p>
+                  </div>
+                  <div className="card-footer">
+                    <button className="btn btn-blue btn-sm m-1">
+                      <Link
+                        to="/rooms/furniture"
+                        state={room.id}
+                        style={{ textDecoration: "none", color: "white" }}
+                      >
+                        Furniture
+                      </Link>
+                    </button>
+
+                    <Button
+                      variant="btn-sm m-1"
+                      className="btn-red btn-sm text-right"
+                      onClick={() => {
+                        Swal.fire({
+                          title: "Are you sure?",
+                          text: "You won't be able to revert this!",
+                          icon: "warning",
+                          showCancelButton: true,
+                          confirmButtonColor: "#bb7e74",
+                          cancelButtonColor: "#808080",
+                          confirmButtonText: "Yes, delete it!",
+                        }).then((result) => {
+                          if (result.value) {
+                            deletion(room.id);
+                            Swal.fire(
+                              "Deleted!",
+                              "Your room has been deleted.",
+                              "success"
+                            );
+                          }
+                        });
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </div>
                 </div>
               </div>
-              <img
-                src={room.picture_url}
-                className="card-img-top card-image"
-                alt="Image"
-              />
-              <div className="card-body">
-                <h5 className="card-title">{room.name}</h5>
-                <p className="card-text crop-text-1">{room.description}</p>
-              </div>
-            </div>
-          </div>
-        ))}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
